a4166cdb039c1bfc09c360060c2e41902def3532
Yugandhar Pathi <yugandhar.pathi@hpe.com>
2020-04-24 18:38:50 -0700

    updat test steps

--- src/routes/details-page.js
+++ src/routes/details-page.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import { Text } from 'grommet';
 import { Link } from 'react-router-dom';
+import { Pan } from 'grommet-icons';
+
+const ListItem = ({ children }) => {
+  return (
+    <li>
+      <Text>{children}</Text>
+    </li>
+  );
+};
 
 const DetailsPage = () => {
   return (
     <>
-      <Text>You are on details page.</Text>
-      <Link to='/'>Home Page</Link>
+      <Text size='xlarge' weight='bold'>
+        Steps:
+      </Text>
+      <ul>
+        <ListItem>Click on Chat Now button on bottom right.</ListItem>
+        <ListItem>It will bring up a chat window.</ListItem>
+        <ListItem>Displays an Button to Chat with LiveAgent.</ListItem>
+        <ListItem>
+          Click on It ?? No!! We are not in production yet!! we don't have
+          customer care waiting, to chat with you!!.
+        </ListItem>
+        <ListItem>
+          Then how do I chat ? You need to request salesforce developer to
+          accept your request.
+        </ListItem>
+        <ListItem>
+          How do I request? Ping Abdul (abdul.khader@hpe.com) on skype. Abdul
+          works from San Jose, so you need to ping him in PST timings (not on wk
+          ends, ok !!).
+        </ListItem>
+        <ListItem>
+          Once Abdul says, YES!! Go ahead and click on .. Chat with LiveAgent.
+        </ListItem>
+      </ul>
+      <Text size='xlarge' weight='bold'>
+        I didn't Read your steps, my chatbot shows some error page:
+      </Text>
+      <ul>
+        <ListItem>
+          We are in prototype phase you may see some errors in chatbot.
+        </ListItem>
+        <ListItem>How do I bring chatbot back to initial state?.</ListItem>
+        <ListItem>
+          Just refresh your page!! I will re-initialize your chatbot.
+        </ListItem>
+      </ul>
+      <Link to='/'>Back to Home Page</Link>
+
+      <Text size='xlarge' weight='bold' margin={{ top: 'medium' }}>
+        What else can I test?
+      </Text>
+      <ul>
+        <ListItem>You can test a lot!!.</ListItem>
+        <ListItem>You can test dragging feature of Chat window!!</ListItem>
+        <ListItem>
+          Click on {<Pan size='medium' />}icon and move the chat window around.
+        </ListItem>
+        <ListItem>
+          Keep the chatbot open and switch between HomePage and DetailsPage.
+          Your chat window should be as it is.
+        </ListItem>
+      </ul>
     </>
   );
 };
--- src/routes/home-page.js
+++ src/routes/home-page.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text } from 'grommet';
+import { Text, Markdown, Box } from 'grommet';
 import { Link } from 'react-router-dom';
+
+const markdownContent = `
+
+## Goal - Prototype chat bot experience
+
+- Have Chat-button on bottom right corner of the App which can launch chat window.
+- Chat button should exist in the same position when user navigates between pages.
+- Chat button should launch chat window which will have capability to connect with salesforce liveagent.
+- Chat Window stays on the app when user switches between pages.
+- Chat window should be draggable and allow user to view what is behind the window.
+
+`;
 
 const HomePage = () => {
   return (
     <>
-      <Text>You are on home page</Text>
-      <Link to='/details'>Details Page</Link>
+      <Text size='xlarge' weight='bold'>
+        Welcome!
+      </Text>
+      <Box margin={{ top: 'small' }}>
+        <Link to='/details'>How do I test this?</Link>
+      </Box>
+
+      <Markdown>{markdownContent}</Markdown>
     </>
   );
 };
